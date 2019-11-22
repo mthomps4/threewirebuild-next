3d986e359e173a4a7ea308e39a94f2c169d0b2d3
Matt Thompson <matt@echobind.com>
2019-11-22 10:34:46 -0500

    Demo

--- pages/example.tsx
+++ pages/example.tsx
@@ -1,53 +1,10 @@
-import { Column, Container, Label, Row, Spacer, Text, TextInput } from '@3wirebuild/styled-system';
+import { Column, Container, Row, Spacer, Text } from '@3wirebuild/styled-system';
 import { NextPage } from 'next';
 import * as React from 'react';
 
 const ExamplePage: NextPage = () => {
   return (
     <Container>
-      <Container>
-        <h1>CONTAINER EXAMPLES</h1>
-        <Container textColor="blue" border="1px solid blue">
-          I'm a styled container with blue font!
-        </Container>
-        <Container bg="navy" textColor="white" m={3}>
-          Container with margin
-        </Container>
-        <Container bg="black" textColor="white" p={3}>
-          Container with padding
-        </Container>
-        <Container bg="primary" textColor="white" p={4} width={{ _: 1, sm: 0.75, md: 1 / 2, lg: 1 / 4, xl: 1 / 8 }}>
-          Media Container: <br />
-          xl=.125 <br />
-          lg= .25 <br />
-          md=.5 <br />
-          sm =.75 <br />
-          _=1
-        </Container>
-        <p>Flex Row:</p>
-        <Container variant="row" bg="navy" textColor="white" m={2} justifyContent="space-around">
-          <p>Item 1</p>
-          <p>Item 2</p>
-          <p>Item 3</p>
-        </Container>
-        <Row bg="purple" textColor="white" m={2} justifyContent="space-around">
-          <p>Item 1</p>
-          <p>Item 2</p>
-          <p>Item 3</p>
-        </Row>
-        <p>Flex Column:</p>
-        <Container variant="column" bg="navy" textColor="white" m={2}>
-          <p>Item 1</p>
-          <p>Item 2</p>
-          <p>Item 3</p>
-        </Container>
-        <p>Center Content:</p>
-        <Container bg="purple" centerContent textColor="white" m={2}>
-          <p>Item 1</p>
-          <p>Item 2</p>
-          <p>Item 3</p>
-        </Container>
-      </Container>
       <Container>
         <h1>TEXT EXAMPLES:</h1>
         <Text>I'm a normal text component</Text>
@@ -66,16 +23,51 @@ const ExamplePage: NextPage = () => {
         <Text warningBox>I'm a warning box</Text>
         <Text dangerBox>I'm a danger box</Text>
       </Container>
-      <Container>
-        <Label htmlFor="username">Username:</Label>
-        <TextInput name="username" />
-      </Container>
 
-      <Container display="flex" justifyContent="center">
-        <Column width={1 / 2}>
-          <Label htmlFor="username">Username:</Label>
-          <TextInput name="username" />
+      <Container>
+        <h1>CONTAINER EXAMPLES</h1>
+        <Container border="1px solid blue">Blue Border!</Container>
+        <Container bg="primary" color="white" m={3}>
+          With Margin
+        </Container>
+        <Container bg="primary" color="white" p={3}>
+          Container with padding
+        </Container>
+        <Spacer m={3} />
+        <Container bg="primary" color="white" p={4} width={{ _: 1, sm: 0.75, md: 1 / 2, lg: 1 / 4, xl: 1 / 8 }}>
+          Testing Breakpoints: <br />
+          xl=.125 <br />
+          lg= .25 <br />
+          md=.5 <br />
+          sm =.75 <br />
+          _=1
+        </Container>
+        <h2>Row:</h2>
+        <Container variant="row" bg="navy" color="white" m={2} justifyContent="space-around">
+          <p>Item 1</p>
+          <p>Item 2</p>
+          <p>Item 3</p>
+        </Container>
+        <Row bg="purple" color="white" m={2} justifyContent="space-around">
+          <p>Item 1</p>
+          <p>Item 2</p>
+          <p>Item 3</p>
+        </Row>
+        <h2>Column:</h2>
+        <Column bg="navy" color="white" m={2}>
+          <p>Item 1</p>
+          <p>Item 2</p>
+          <p>Item 3</p>
         </Column>
+        <h2>Boolean Props:</h2>
+        <Text fontWeight="bold">centerContent</Text>
+        <Row centerContent>
+          <Row width="50%" bg="purple" justifyContent="space-around" color="white" m={2}>
+            <p>Item 1</p>
+            <p>Item 2</p>
+            <p>Item 3</p>
+          </Row>
+        </Row>
       </Container>
     </Container>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,42 @@
-import { Button, Container, Label, TextInput } from '@3wirebuild/styled-system';
+import { Button, Card, Column, Container, Label, Row, TextInput } from '@3wirebuild/styled-system';
 import { NextPage } from 'next';
+import { useRouter } from 'next/router';
 import * as React from 'react';
 
 const IndexPage: NextPage = () => {
+  const router = useRouter();
+  const navigateToExample = (e: any) => {
+    e.preventDefault();
+    router.push('/example');
+  };
   return (
-    <Container bg="primary">
-      <Container centerContent>
-        <Container>
-          <Label htmlFor="username">Username:</Label>
-          <TextInput name="username" placeholder="username" />
-          <Button>Login</Button>
-        </Container>
-      </Container>
+    <Container centerContent>
+      <Card m={5} bg="primay">
+        <Column>
+          <Row centerContent>
+            <Label pr={2} htmlFor="username" width="40%">
+              Username:
+            </Label>
+            <TextInput py={1} px={2} name="username" placeholder="username" />
+          </Row>
+
+          <Row mt={4} centerContent>
+            <Label pr={2} htmlFor="password" width="40%">
+              Password:
+            </Label>
+            <TextInput py={1} px={2} name="password" type="password" placeholder="********" />
+          </Row>
+
+          <Row mt={4} justifyContent="space-between">
+            <Button width="40%" disabled>
+              Disabled
+            </Button>
+            <Button onClick={navigateToExample} color="white" bg="primary" width="40%">
+              Login
+            </Button>
+          </Row>
+        </Column>
+      </Card>
     </Container>
   );
 };
